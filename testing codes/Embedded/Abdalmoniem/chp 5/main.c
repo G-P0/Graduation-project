06fe718ebee78b687aafc1c8c33949aaa52f553f
unknown <abdalmoniem.sami211@el-eng.menofia.edu.eg>
2018-02-24 10:41:41 +0200

    testing git in atom - ignore commit -

--- testing codes/Embedded/Abdalmoniem/chp 5/main.c
+++ testing codes/Embedded/Abdalmoniem/chp 5/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void subarray(int [],int );
-#define soa(a) sizeof(a)/sizeof(a[0]) 
+#define soa(a) sizeof(a)/sizeof(a[0])
 #define sub(x) &arr[(int)x],size-(int)x
 int main(int argc, char const *argv[])
 {
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 	for (int i = 0; i<size; ++i)
 	{
 		arr[i]=i;
-		
+
 	}
 	printf("%d\n", size );
 	subarray(sub('A'));
@@ -19,7 +19,9 @@ int main(int argc, char const *argv[])
 
 void subarray (int a[], int size)
 {
+
 	
+
 	for (int i = 0; i < size; ++i)
 	{
 		printf("%d ",*(a+i) );

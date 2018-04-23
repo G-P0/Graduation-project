776e2e47dd06bc194a05c2b18992e8cd7ff0387b
unknown <abdalmoniem.sami211@el-eng.menofia.edu.eg>
2018-04-23 17:06:42 +0200

    fixed bugs in validation

---
+++ testing codes/Web/zahraa/smartHomeTest/views/html_header.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    
+	    <!-- Custom fonts - Font Awesome -->
+	    <link rel="stylesheet" type="text/css" href="resources/css/font-awesome.min.css">
+	    <!-- Bootstrap core CSS --> 
+	    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+	    <!-- Custom styles -->
+	    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+	    <!-- Google Fonts -->
+	    <link href="https://fonts.googleapis.com/css?family=Saira+Condensed:400,500" rel="stylesheet">
